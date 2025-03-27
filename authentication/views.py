8bbfd54759bcf7d2da5c74ca748307e33e5d1ab4
Emir <148708322+eeemmm29@users.noreply.github.com>
2025-03-27 11:53:20 +0600

    Add django-cors-headerst to requirements and fix imports in auth views

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import status, viewsets
-from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from ..authentication.serializers import UserProfileSerializer, UserSerializer
+from authentication.serializers import UserSerializer
+
 from .models import UserProfile
 
 
