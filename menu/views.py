585e1cfa4c6095b333de41b440d39958dd26754e
Emir <148708322+eeemmm29@users.noreply.github.com>
2025-04-24 17:01:52 +0600

    Revert "Be able to only get favorited menu items for a user"
    This reverts commit c6ea620c219c49f8c868181c9522ec891b15efad.

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets
 
-from .filters import MenuItemFilter
 from .models import Category, MenuItem
 from .serializers import CategorySerializer, MenuItemSerializer
 
@@ -20,7 +19,7 @@ class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_class = MenuItemFilter  # Use the custom FilterSet
+    filterset_fields = ["category", "is_available"]
 
     def get_permissions(self):
         if self.action in ["create", "update", "partial_update", "destroy"]:
