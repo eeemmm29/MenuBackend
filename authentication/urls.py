545b7fc9428a4038b2d82425d972402091649351
Emir <148708322+eeemmm29@users.noreply.github.com>
2025-04-24 17:49:13 +0600

    Add slashed to the ends of auth urls
    They were there by default actually

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,10 +5,10 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
-    path("register", RegisterView.as_view(), name="rest_register"),
-    path("login", LoginView.as_view(), name="rest_login"),
-    path("logout", LogoutView.as_view(), name="rest_logout"),
-    path("user", UserDetailsView.as_view(), name="rest_user_details"),
-    path("token/verify", TokenVerifyView.as_view(), name="token_verify"),
-    path("token/refresh", get_refresh_view().as_view(), name="token_refresh"),
+    path("register/", RegisterView.as_view(), name="rest_register"),
+    path("login/", LoginView.as_view(), name="rest_login"),
+    path("logout/", LogoutView.as_view(), name="rest_logout"),
+    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
 ]
